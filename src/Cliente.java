7e5383b70451bba98aabbd76956a56282cb1f8a6
Amanda Paiva Lino <106124434+Amandapvln@users.noreply.github.com>
2023-02-19 11:32:58 -0300

    atualização

---
+++ src/Cliente.java
@@ -0,0 +1,42 @@
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String endereco;
+
+    public Cliente(String nome, String cpf, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String toString() {
+        return "Nome: " + nome + "\n" +
+               "CPF: " + cpf + "\n" +
+               "Endereço: " + endereco;
+    }
+}
