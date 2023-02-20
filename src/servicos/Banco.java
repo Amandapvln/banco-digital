725c79cae004f21ed3183cc4d561ef79ef56e0ac
Amanda Paiva Lino <106124434+Amandapvln@users.noreply.github.com>
2023-02-20 14:28:18 -0300

    att

---
+++ src/servicos/Banco.java
@@ -0,0 +1,22 @@
+package servicos;
+import java.util.List;
+
+import entidades.Conta;
+
+public class Banco {
+	private String nome;
+	private List<Conta> contas;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Conta> getContas() {
+		return contas;
+	}
+	
+}
